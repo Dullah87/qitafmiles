134127f0ee3c2fa1b8f3a1adfcd43f5b606b20bd
Dullah87 <oboudi.a@gmail.com>
2023-09-04 22:36:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const calculateBtn = document.getElementById("calculateBtn");
-const resultElement = document.getElementById("result");
+const qitafPointsElement = document.getElementById("qitafPoints");
+const qitafCostElement = document.getElementById("qitafCost");
+const saudiaMilesCostElement = document.getElementById("saudiaMilesCost");
 
 calculateBtn.addEventListener("click", () => {
-    const flightCost = parseFloat(document.getElementById("flightCost").value);
-    const milesCost = parseFloat(document.getElementById("milesCost").value);
+    const milesInput = parseFloat(document.getElementById("milesInput").value);
 
-    if (isNaN(flightCost) || isNaN(milesCost)) {
-        resultElement.textContent = "Please enter valid numbers.";
+    if (isNaN(milesInput) || milesInput <= 0) {
+        alert("Please enter a valid positive number of miles.");
         return;
     }
 
-    const totalCostWithoutMiles = flightCost;
-    const totalCostWithMiles = flightCost + milesCost;
+    const qitafPointsNeeded = milesInput / 5;
+    const qitafCost = qitafPointsNeeded * 0.35;
+    const saudiaMilesCost = 0.129398 * milesInput;
 
-    resultElement.textContent = `Total Cost Without Miles: $${totalCostWithoutMiles.toFixed(2)}
-                                  Total Cost With Miles: $${totalCostWithMiles.toFixed(2)}`;
+    qitafPointsElement.textContent = qitafPointsNeeded.toFixed(2);
+    qitafCostElement.textContent = `$${qitafCost.toFixed(2)}`;
+    saudiaMilesCostElement.textContent = `$${saudiaMilesCost.toFixed(2)}`;
 });
