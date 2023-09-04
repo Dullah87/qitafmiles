10b65ee5c52487b763dc11f0f1dc443ae972a97a
Dullah87 <oboudi.a@gmail.com>
2023-09-04 22:39:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ const qitafCostElement = document.getElementById("qitafCost");
 const saudiaMilesCostElement = document.getElementById("saudiaMilesCost");
 
 calculateBtn.addEventListener("click", () => {
+    console.log("Button clicked"); // Debug line
+
     const milesInput = parseFloat(document.getElementById("milesInput").value);
+
+    console.log("Miles input:", milesInput); // Debug line
 
     if (isNaN(milesInput) || milesInput <= 0) {
         alert("Please enter a valid positive number of miles.");
@@ -14,6 +18,10 @@ calculateBtn.addEventListener("click", () => {
     const qitafPointsNeeded = milesInput / 5;
     const qitafCost = qitafPointsNeeded * 0.35;
     const saudiaMilesCost = 0.129398 * milesInput;
+
+    console.log("Qitaf points:", qitafPointsNeeded); // Debug line
+    console.log("Qitaf cost:", qitafCost); // Debug line
+    console.log("Saudia miles cost:", saudiaMilesCost); // Debug line
 
     qitafPointsElement.textContent = qitafPointsNeeded.toFixed(2);
     qitafCostElement.textContent = `$${qitafCost.toFixed(2)}`;
